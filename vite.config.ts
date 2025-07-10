c246b8bf500e6c744f789730f8e1c46c55bf130d
Alex9600t <dukovaleks2@gmail.com>
2025-07-10 18:42:36 +0300

    fix(CI/CD)

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: '[name]-[hash][extname]',
-        chunkFileNames: '[name]-[hash].js',
-        entryFileNames: '[name]-[hash].js',
+        assetFileNames: '[name][extname]',
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js',
       }
     }
   }
